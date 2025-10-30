19357521f09630329d6c0c49197b64709a5f6614
Richin Mrudul <richinm@gmail.com>
2025-10-30 12:19:06 -0400

    footer comp

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center py-6 mt-20">
+      <p className="text-gray-600">
+        © {new Date().getFullYear()} Purdue CS Club — All Rights Reserved
+      </p>
+    </footer>
+  );
+}
